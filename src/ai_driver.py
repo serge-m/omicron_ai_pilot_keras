6f43c8035a6206f6ec95473f41ac79698a323939
kurbakov <kurbakov.d@gmail.com>
2019-03-29 19:45:32 +0100

    initial commit

---
+++ src/ai_driver.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import time
+
+import rospy
+from ackermann_msgs.msg import AckermannDriveStamped
+
+
+class ROSPackage_AI_Driver:
+	def __init__(self):
+		rospy.init_node('ai_driver')
+		
+		self.value_to_publish = AckermannDriveStamped()
+		self.value_to_publish.drive.speed = 1.0
+		self.value_to_publish.drive.steering_angle = 0.0
+		self.ai_driver_publisher = rospy.Publisher('ackermann_cmd_mux/input/ai_driver', AckermannDriveStamped, queue_size=10)
+
+	def start(self):
+		self.ai_driver_publisher.publish(self.value_to_publish)
+
+if __name__ == '__main__':
+	package = ROSPackage_AI_Driver()
+	package.start()
