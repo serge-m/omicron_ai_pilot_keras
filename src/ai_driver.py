c3a13d5008d9c32c3e2569dd778a4e4f28a09c01
Michal Slonina <michal.slonina@gmail.com>
2019-05-26 19:25:56 +0000

    pep8 ai_driver.py

--- src/ai_driver.py
+++ src/ai_driver.py
@@ -7,24 +7,25 @@ from ackermann_msgs.msg import AckermannDriveStamped
 
 
 class ROSPackage_AI_Driver:
-	def __init__(self):
-		rospy.init_node('ai_driver')
-		
-		self.value_to_publish = AckermannDriveStamped()
-		self.value_to_publish.drive.speed = 1.0
-		self.value_to_publish.drive.steering_angle = 0.0
-		self.ai_driver_publisher = rospy.Publisher('ackermann_cmd_mux/input/ai_driver', AckermannDriveStamped, queue_size=10)
+    def __init__(self):
+        rospy.init_node('ai_driver')
 
-	def start(self):
-		rate = rospy.Rate(10)
-		while not rospy.is_shutdown():
-			self.ai_driver_publisher.publish(self.value_to_publish)
-			rate.sleep()
+        self.value_to_publish = AckermannDriveStamped()
+        self.value_to_publish.drive.speed = 1.0
+        self.value_to_publish.drive.steering_angle = 0.0
+        self.ai_driver_publisher = rospy.Publisher(
+            'ackermann_cmd_mux/input/ai_driver', AckermannDriveStamped, queue_size=10)
+
+    def start(self):
+        rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            self.ai_driver_publisher.publish(self.value_to_publish)
+            rate.sleep()
 
 
 if __name__ == '__main__':
-	package = ROSPackage_AI_Driver()
-	try:
-		package.start()
-	except rospy.ROSInterruptException:
-		pass
+    package = ROSPackage_AI_Driver()
+    try:
+        package.start()
+    except rospy.ROSInterruptException:
+        pass
