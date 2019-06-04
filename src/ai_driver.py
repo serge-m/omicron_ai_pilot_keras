d624fe2ffb063b6ab407b0949894eed325ea1692
Michal Slonina <michal.slonina@gmail.com>
2019-06-04 21:34:27 +0000

    listen for compressed camera data

--- src/ai_driver.py
+++ src/ai_driver.py
@@ -4,10 +4,12 @@ import time
 
 import rospy
 from ackermann_msgs.msg import AckermannDriveStamped
-
+from sensor_msgs.msg import CompressedImage
 
 class ROSPackage_AI_Driver:
     def __init__(self):
+        rospy.loginfo("AI driver init")
+        print("DUPA")
         rospy.init_node('ai_driver')
 
         self.value_to_publish = AckermannDriveStamped()
@@ -15,14 +17,21 @@ class ROSPackage_AI_Driver:
         self.value_to_publish.drive.steering_angle = 0.0
         self.ai_driver_publisher = rospy.Publisher(
             'ackermann_cmd_mux/input/ai_driver', AckermannDriveStamped, queue_size=10)
+        rospy.Subscriber('raspicam_node/image/compressed', CompressedImage, self.receive_compressed_image)
 
     def start(self):
-        rate = rospy.Rate(10)
-        while not rospy.is_shutdown():
-            self.ai_driver_publisher.publish(self.value_to_publish)
-            rate.sleep()
+        rospy.loginfo("AI driver start")
+        rospy.spin()
+        #rate = rospy.Rate(10)
+        #while not rospy.is_shutdown():
+            #self.ai_driver_publisher.publish(self.value_to_publish)
+            #rate.sleep()
 
+    def receive_compressed_image(self, img):
+        rospy.loginfo("img",img)
+        self.ai_driver_publisher.publish(self.value_to_publish)
 
+import sys
 if __name__ == '__main__':
     package = ROSPackage_AI_Driver()
     try:
