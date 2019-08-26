2a98593ae5916147fa54b988e19cb0a8dd93f4be
SergeM <sbmatyunin@gmail.com>
2019-08-26 23:56:30 +0200

    Fixed threading problems in ai_driver, path to model is a parameter

--- src/ai_driver.py
+++ src/ai_driver.py
@@ -1,59 +1,79 @@
 #!/usr/bin/env python3
-
-import time
+import threading
 
 import rospy
 from ackermann_msgs.msg import AckermannDriveStamped
 from sensor_msgs.msg import CompressedImage
 
-from keras.models import load_model
+import tensorflow as tf
+from tensorflow.python.keras.models import load_model
 from keras_preprocessing.image.utils import load_img, img_to_array
 
-from PIL import Image as pil_image
 from io import BytesIO
 import numpy as np
+import sys
+import os
 
-class ROSPackage_AI_Driver:
-    def __init__(self):
+import tensorflow.keras as K
+
+
+class TFModel:
+    def __init__(self, path):
+        self.path = path
+        self.model = load_model(self.path)
+        image = np.zeros([1, 120, 160, 3])
+        # warm-up run to prepare for multi-threading
+        self.model.predict(image)
+        self.session = K.backend.get_session()
+        self.graph = tf.get_default_graph()
+        self.graph.finalize()  # finalize
+
+    def predict(self, image):
+        with self.session.as_default():
+            with self.graph.as_default():
+                prediction = self.model.predict(image)
+        return prediction
+
+
+def create_message(speed, angle):
+    message = AckermannDriveStamped()
+    message.drive.speed = speed
+    message.drive.steering_angle = angle
+    return message
+
+
+class FixedAIDriver:
+    def __init__(self, path):
         rospy.init_node('ai_driver')
-        rospy.loginfo("AI driver init. Loading neural network...")
-        self.model = load_model('/tmp/best_model.h5')
-        rospy.loginfo("Neural network loaded, starting service.")
-
-        self.value_to_publish = AckermannDriveStamped()
-        self.value_to_publish.drive.speed = 1.0
-        self.value_to_publish.drive.steering_angle = 0.0
-        self.ai_driver_publisher = rospy.Publisher(
-            'ackermann_cmd_mux/input/ai_driver', AckermannDriveStamped, queue_size=10)
+        self.model = TFModel(path)
         rospy.Subscriber('raspicam_node/image/compressed', CompressedImage, self.receive_compressed_image)
+        self.ai_driver_publisher = rospy.Publisher('ackermann_cmd_mux/input/ai_driver', AckermannDriveStamped,
+                                                   queue_size=10)
 
     def start(self):
         rospy.loginfo("AI driver start")
-        rospy.spin()
-        #rate = rospy.Rate(10)
-        #while not rospy.is_shutdown():
-            #self.ai_driver_publisher.publish(self.value_to_publish)
-            #rate.sleep()
+        while not rospy.is_shutdown():
+            rospy.spin()
+        rospy.loginfo("AI driver finished")
 
     def image_to_array(self, img):
-        return img_to_array(load_img(BytesIO(img), target_size=(32,32)))
+        return img_to_array(load_img(BytesIO(img), target_size=(120, 160)))
 
     def receive_compressed_image(self, img):
-        #rospy.loginfo("img" + type(img).__name__)
-        #rospy.loginfo("img" + str(len(img.data)))
-        image = self.image_to_array(img.data)
-        image=np.expand_dims(image,0)
+        rospy.loginfo("PID %d, thread %d ", os.getpid(), threading.get_ident())
+        rospy.loginfo("img %s size %d", type(img).__name__, len(img.data))
+        image = self.image_to_array(img.data)[np.newaxis]
         prediction = self.model.predict(image)
-        #rospy.loginfo(prediction[0])
-        value = (np.argmax(prediction[0]) - 2) / 2
-        #rospy.loginfo(str(value))
-        self.value_to_publish.drive.steering_angle = value
-        self.ai_driver_publisher.publish(self.value_to_publish)
 
-import sys
+        rospy.loginfo(prediction[0])
+        self.ai_driver_publisher.publish(create_message(speed=0.5 + img.data[12120] * 0.001, angle=prediction[0]))
+
+
 if __name__ == '__main__':
-    package = ROSPackage_AI_Driver()
     try:
-        package.start()
-    except rospy.ROSInterruptException:
-        pass
+        path_model = sys.argv[1]
+    except IndexError:
+        path_model = '/tmp/model'
+
+    package = FixedAIDriver(path_model)
+    package.start()
