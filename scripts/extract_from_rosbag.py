9033dd5ac76482b7e87e8a2d72e463f88f6d690b
SergeM <sbmatyunin@gmail.com>
2019-10-20 11:38:31 +0200

    more customizable extraction from rosbag, add extraction notebook for many bags

--- scripts/extract_from_rosbag.py
+++ scripts/extract_from_rosbag.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
+from __future__ import print_function
 import argparse
 import os
 import csv
+import sys
 
 import pandas
 import cv2
@@ -14,17 +16,9 @@ js_topic = "/pwm_radio_arduino/radio_pwm"
 image_topic = "/raspicam_node/image/compressed"
 
 
-def dir_exists(dir):
-    return os.path.exists(dir)
-
-
-def create_dir(dir):
-    os.mkdir(dir)
-
-
 def extract_data(rosbag_path, dir_path):
     bag = rosbag.Bag(rosbag_path, 'r')
-    with open(os.path.join(dir_path, 'data_js.csv'), 'w') as writeFile:
+    with open(path_steering(dir_path), 'w') as writeFile:
         writer = csv.writer(writeFile)
         writer.writerow(["timestamp",
                          "msg.steering", "msg.throttle"])
@@ -34,7 +28,7 @@ def extract_data(rosbag_path, dir_path):
 
     bridge = CvBridge()
     counter = 0
-    with open(os.path.join(dir_path, 'data_image.csv'), 'w') as writeFile:
+    with open(path_image_list(dir_path), 'w') as writeFile:
         writer = csv.writer(writeFile)
         writer.writerow(["timestamp", "msg.header.seq", "msg.header.stamp.secs", "msg.header.stamp.nsecs", "image"])
         for topic, msg, t in bag.read_messages(topics=[image_topic]):
@@ -48,18 +42,26 @@ def extract_data(rosbag_path, dir_path):
     bag.close()
 
 
-def match_data(directory):
-    print(os.path.join(directory, "data_js.csv"))
-    js4_data = pandas.read_csv(os.path.join(directory, "data_js.csv"))
-    image_data = pandas.read_csv(os.path.join(directory, "data_image.csv"))
+def path_image_list(dir_path):
+    return os.path.join(dir_path, 'data_image.csv')
 
-    js4_data.sort_values("timestamp")
-    image_data.sort_values("timestamp")
 
-    print(os.path.join(directory, 'data.csv'))
-    with open(os.path.join(directory, 'data.csv'), 'w') as writeFile:
+def path_steering(dir_path):
+    return os.path.join(dir_path, 'data_js.csv')
+
+
+def match_data(data_dir, dst_csv_path):
+    steering_data = pandas.read_csv(path_steering(data_dir))
+    image_data = pandas.read_csv(path_image_list(data_dir))
+
+    steering_data = steering_data.sort_values("timestamp")
+    image_data = image_data.sort_values("timestamp")
+
+    relative_dir_of_images = os.path.relpath(data_dir, start=os.path.dirname(dst_csv_path))
+
+    with open(dst_csv_path, 'w') as writeFile:
         writer = csv.writer(writeFile)
-        iter = js4_data.iterrows()
+        iter = steering_data.iterrows()
         data = None
 
         search = True
@@ -74,18 +76,43 @@ def match_data(directory):
                 if image_row["timestamp"] < data["timestamp"]:
                     break
 
-            writer.writerow([image_row["image"], data["msg.steering"], data["msg.throttle"]])
+            writer.writerow((
+                os.path.join(relative_dir_of_images, image_row["image"]),
+                data["msg.steering"],
+                data["msg.throttle"]
+            ))
+
+
+def main(in_rosbag, out_csv, out_dir, overwrite):
+    """
+    :param in_rosbag: location of the input rosbag
+    :param out_csv: location of the output csv with relative paths to images and steering
+    :param out_dir: directory to write images
+    :param overwrite: flag, if the data should be overwritten. The function raises IOError if output exists and
+        overwrite is false
+    """
+    if os.path.exists(out_csv) and not overwrite:
+        raise IOError("Error: output csv exists")
+    if os.path.exists(out_dir):
+        if not overwrite:
+            raise IOError("Error: output directory exists")
+    else:
+        os.makedirs(out_dir)
+    extract_data(in_rosbag, out_dir)
+    match_data(out_dir, out_csv)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='description')
     parser.add_argument('-rb', type=str, required=True, help='path to the rosbag')
     parser.add_argument('-d', type=str, required=True, help='directory to store the data')
-
+    parser.add_argument('--csv', '-o', type=str, required=False, help='path to the compiled csv')
+    parser.add_argument('--overwrite', '-y', action='store_true', help='overwrite if directory exists')
     args = parser.parse_args()
+    dst_csv_path = args.csv if args.csv is not None else os.path.join(args.d, 'data.csv')
 
-    if not dir_exists(args.d):
-        create_dir(args.d)
-
-    extract_data(args.rb, args.d)
-    match_data(args.d)
+    try:
+        main(args.rb, dst_csv_path, args.d, args.overwrite)
+    except IOError as e:
+        print("Error: {}".format(e), file=sys.stderr)
+        exit(1)
